eb38702b6525553c447a007b0c1a2de7581419ed
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2024-06-09 16:54:24 +0300

    Add files via upload

---
+++ WpfExample/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private InfoCommand _infoCommand = new InfoCommand();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Свържете DataContext с текущия MainWindow
+            this.DataContext = this;
+        }
+
+        // Свойство за публичен достъп до командата
+        public InfoCommand InformationCommand
+        {
+            get { return _infoCommand; }
+        }
+    }
+}
---
+++ WpfExample/NamesList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace WpfExample
+{
+    public class NamesList : INotifyPropertyChanged
+    {
+        string _firstName = "";
+        string _lastName = "";
+        string _selectedName;
+        public NamesList()
+        {
+            Names = new ObservableCollection<string>();
+            AddNameCommand = new AddCommand();
+            RemoveNameCommand = new RemoveCommand();
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (_firstName != value)
+                {
+                    _firstName = value;
+                    OnPropertyChanged("FirstName");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                if (_lastName != value)
+                {
+                    _lastName = value;
+                    OnPropertyChanged("LastName");
+                }
+            }
+        }
+
+        public string SelectedName
+        {
+            get { return _selectedName; }
+            set
+            {
+                if (_selectedName != value)
+                {
+                    _selectedName = value;
+                    OnPropertyChanged("SelectedName");
+                }
+            }
+        }
+
+        public ObservableCollection<string> Names { get; private set; }
+
+        public ICommand AddNameCommand { get; private set; }
+        public ICommand RemoveNameCommand { get; private set; }
+
+        private void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
